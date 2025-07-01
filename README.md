3804ca8fed3d7bc23fe884d19cc467d7717ca07b
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-07-01 16:06:17 +0530

    update

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ This is the first project which i am going to make using django to practice what
 To connect the frontend to the backend , we need to use a method which is actually a post method.
 
 - method="post"
+### This is used to get the data from frontend to the backend
+### But vice versa is done using the "context" attribute.
+
+also we use "request.POST" in the views.
+
+Next we need to give something like this {% csrf_token %}
+Django uses this to check and verify whether the request to get the frontend data to the backend is coming from this or our own currently used server or not.
+
+Its kind of a security stuff/protocol.
--- recipe/khana/views.py
+++ recipe/khana/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import *
 from django.http import HttpResponse
 
 def home(request):
     return  HttpResponse("This is the home page of the 1st django practice project.")
 
 def recipies(request):
-    
+    if request.method=="POST":
+
+      data=request.POST
+      recipe_image=request.FILES.get('recipe_image')
+      recipe_name=data.get('recipe_name')
+      recipe_description=data.get('recipe_description')
+     
+      
+      recipe.objects.create (
+         recipe_name=recipe_name,
+         recipe_description=recipe_description,
+         recipe_image=recipe_image
+      )
+
+      return redirect('/recipies')
+
     return render(request, "recipies.html")
