9b00937b6f700996ea3d5086552d6dbf6856c136
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-07-02 16:42:04 +0530

    update

--- recipe/recipe/urls.py
+++ recipe/recipe/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from khana.views import *
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('', home),
     path('admin/', admin.site.urls),
@@ -24,3 +27,7 @@ urlpatterns = [
    
 ] 
     
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns+=staticfiles_urlpatterns()
